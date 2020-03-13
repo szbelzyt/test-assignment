48515408cfb02734ebc674428d1759e907f87a36
Krzysztof Zdulski <krzys.zdulski@gmail.com>
2020-03-13 18:01:06 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+import numpy as np
+import min
+
+def test_min():
+  assert min([1,2,3,4,5]) == 1
+  assert min([4,5,6,2,1,2]) == 1
+  assert min([5,6,3,-10,2]) == -10
+  assert type(min([2,3,3,3,])).__module__ == np.__module__
