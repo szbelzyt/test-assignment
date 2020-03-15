3abbfb64f342b961f14fc7c2eea8a66d51cfe29e
Krzysztof Zdulski <krzys.zdulski@gmail.com>
2020-03-15 21:20:13 +0100

    Update task1.py

--- task1.py
+++ task1.py
@@ -5,5 +5,6 @@ def rectangle_area(a, b):
     :param a: first side of the rectangle
     :param b: second side of the rectangle
     :return: area of the rectangle
+    :raises ValueError if either number was negative
     """
     pass
